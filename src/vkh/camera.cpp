0869a0d33dbec2df91ff7f6454f0d8bab3442788
kekw <pboursin2008@orange.fr>
2024-10-23 18:15:38 +0200

    refactored the world, removed obj dupes, fixed anger issues against the arch that once was ruling vulkhan

--- src/vkh/camera.cpp
+++ src/vkh/camera.cpp
@@ -6,8 +6,11 @@
 
 namespace vkh {
 
-void LveCamera::setOrthographicProjection(
-    float left, float right, float top, float bottom, float near, float far) {
+Camera::Camera(glm::vec3 position, glm::vec3 rotation)
+    : position(position), rotation(rotation) {}
+
+void Camera::calcOrthographicProjection(float left, float right, float top,
+                                        float bottom, float near, float far) {
   projectionMatrix = glm::mat4{1.0f};
   projectionMatrix[0][0] = 2.f / (right - left);
   projectionMatrix[1][1] = 2.f / (bottom - top);
@@ -17,7 +20,8 @@ void LveCamera::setOrthographicProjection(
   projectionMatrix[3][2] = -near / (far - near);
 }
 
-void LveCamera::setPerspectiveProjection(float fovy, float aspect, float near, float far) {
+void Camera::calcPerspectiveProjection(float fovy, float aspect, float near,
+                                       float far) {
   assert(glm::abs(aspect - std::numeric_limits<float>::epsilon()) > 0.0f);
   const float tanHalfFovy = tan(fovy / 2.f);
   projectionMatrix = glm::mat4{0.0f};
@@ -28,7 +32,7 @@ void LveCamera::setPerspectiveProjection(float fovy, float aspect, float near, f
   projectionMatrix[3][2] = -(far * near) / (far - near);
 }
 
-void LveCamera::setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up) {
+void Camera::calcViewDirection(glm::vec3 direction, glm::vec3 up) {
   const glm::vec3 w{glm::normalize(direction)};
   const glm::vec3 u{glm::normalize(glm::cross(w, up))};
   const glm::vec3 v{glm::cross(w, u)};
@@ -47,7 +51,7 @@ void LveCamera::setViewDirection(glm::vec3 position, glm::vec3 direction, glm::v
   viewMatrix[3][1] = -glm::dot(v, position);
   viewMatrix[3][2] = -glm::dot(w, position);
 
-  inverseViewMatrix = glm::mat4{ 1.f };
+  inverseViewMatrix = glm::mat4{1.f};
   inverseViewMatrix[0][0] = u.x;
   inverseViewMatrix[0][1] = u.y;
   inverseViewMatrix[0][2] = u.z;
@@ -62,19 +66,21 @@ void LveCamera::setViewDirection(glm::vec3 position, glm::vec3 direction, glm::v
   inverseViewMatrix[3][2] = position.z;
 }
 
-void LveCamera::setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up) {
-  setViewDirection(position, target - position, up);
+void Camera::calcViewTarget(glm::vec3 target, glm::vec3 up) {
+  calcViewDirection(target - position, up);
 }
 
-void LveCamera::setViewYXZ(glm::vec3 position, glm::vec3 rotation) {
+void Camera::calcViewYXZ() {
   const float c3 = glm::cos(rotation.z);
   const float s3 = glm::sin(rotation.z);
   const float c2 = glm::cos(rotation.x);
   const float s2 = glm::sin(rotation.x);
   const float c1 = glm::cos(rotation.y);
   const float s1 = glm::sin(rotation.y);
-  const glm::vec3 u{(c1 * c3 + s1 * s2 * s3), (c2 * s3), (c1 * s2 * s3 - c3 * s1)};
-  const glm::vec3 v{(c3 * s1 * s2 - c1 * s3), (c2 * c3), (c1 * c3 * s2 + s1 * s3)};
+  const glm::vec3 u{(c1 * c3 + s1 * s2 * s3), (c2 * s3),
+                    (c1 * s2 * s3 - c3 * s1)};
+  const glm::vec3 v{(c3 * s1 * s2 - c1 * s3), (c2 * c3),
+                    (c1 * c3 * s2 + s1 * s3)};
   const glm::vec3 w{(c2 * s1), (-s2), (c1 * c2)};
   viewMatrix = glm::mat4{1.f};
   viewMatrix[0][0] = u.x;
@@ -90,7 +96,7 @@ void LveCamera::setViewYXZ(glm::vec3 position, glm::vec3 rotation) {
   viewMatrix[3][1] = -glm::dot(v, position);
   viewMatrix[3][2] = -glm::dot(w, position);
 
-  inverseViewMatrix = glm::mat4{ 1.f };
+  inverseViewMatrix = glm::mat4{1.f};
   inverseViewMatrix[0][0] = u.x;
   inverseViewMatrix[0][1] = u.y;
   inverseViewMatrix[0][2] = u.z;
@@ -105,4 +111,4 @@ void LveCamera::setViewYXZ(glm::vec3 position, glm::vec3 rotation) {
   inverseViewMatrix[3][2] = position.z;
 }
 
-}  // namespace lve
+} // namespace vkh
