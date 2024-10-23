1751cfcf7a895a41d879c1599309babf4401930e
kekw <pboursin2008@orange.fr>
2024-10-23 15:46:38 +0200

    more logging, less useless oop, more namespaces

--- src/vkh/systems/point_light_system.cpp
+++ src/vkh/systems/point_light_system.cpp
@@ -13,123 +13,122 @@
 #include <stdexcept>
 
 namespace vkh {
+namespace pointLightSys {
+std::unique_ptr<Pipeline> pipeline;
+VkPipelineLayout pipelineLayout;
+struct PointLightPushConstants {
+  glm::vec4 position{};
+  glm::vec4 color{};
+  float radius;
+};
 
-	struct PointLightPushConstants {
-		glm::vec4 position{};
-		glm::vec4 color{};
-		float radius;
-	};
+void createPipelineLayout(EngineContext &context,
+                          VkDescriptorSetLayout globalSetLayout) {
+  VkPushConstantRange pushConstantRange{};
+  pushConstantRange.stageFlags =
+      VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+  pushConstantRange.offset = 0;
+  pushConstantRange.size = sizeof(PointLightPushConstants);
 
-	PointLightSystem::PointLightSystem(EngineContext& context, VkDescriptorSetLayout globalSetLayout)
-		: context{ context } {
-		createPipelineLayout(globalSetLayout);
-		createPipeline();
-	}
+  std::vector<VkDescriptorSetLayout> descriptorSetLayouts{globalSetLayout};
 
-	PointLightSystem::~PointLightSystem() {
-		vkDestroyPipelineLayout(context.vulkan.device, pipelineLayout, nullptr);
-	}
+  VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+  pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+  pipelineLayoutInfo.setLayoutCount =
+      static_cast<uint32_t>(descriptorSetLayouts.size());
+  pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
+  pipelineLayoutInfo.pushConstantRangeCount = 1;
+  pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
+  if (vkCreatePipelineLayout(context.vulkan.device, &pipelineLayoutInfo,
+                             nullptr, &pipelineLayout) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create pipeline layout!");
+  }
+}
+void createPipeline(EngineContext &context) {
+  assert(pipelineLayout != nullptr &&
+         "Cannot create pipeline before pipeline layout");
 
-	void PointLightSystem::createPipelineLayout(VkDescriptorSetLayout globalSetLayout) {
-		VkPushConstantRange pushConstantRange{};
-		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
-		pushConstantRange.offset = 0;
-		pushConstantRange.size = sizeof(PointLightPushConstants);
+  PipelineConfigInfo pipelineConfig{};
+  Pipeline::enableAlphaBlending(pipelineConfig);
+  pipelineConfig.attributeDescriptions.clear();
+  pipelineConfig.bindingDescriptions.clear();
+  pipelineConfig.renderPass = renderer::getSwapChainRenderPass(context);
+  pipelineConfig.pipelineLayout = pipelineLayout;
+  pipeline = std::make_unique<Pipeline>(
+      context, "pointLightSys", "shaders/point_light.vert.spv", "shaders/point_light.frag.spv",
+      pipelineConfig);
+}
+void init(EngineContext &context, VkDescriptorSetLayout globalSetLayout) {
+  createPipelineLayout(context, globalSetLayout);
+  createPipeline(context);
+}
 
-		std::vector<VkDescriptorSetLayout> descriptorSetLayouts{ globalSetLayout };
+void cleanup(EngineContext &context) {
+  pipeline = nullptr;
+  vkDestroyPipelineLayout(context.vulkan.device, pipelineLayout, nullptr);
+}
 
-		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-		pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
-		pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
-		pipelineLayoutInfo.pushConstantRangeCount = 1;
-		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
-		if (vkCreatePipelineLayout(context.vulkan.device, &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
-			VK_SUCCESS) {
-			throw std::runtime_error("failed to create pipeline layout!");
-		}
-	}
 
-	void PointLightSystem::createPipeline() {
-		assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
+void update(EngineContext &context, FrameInfo &frameInfo, GlobalUbo &ubo) {
+  auto rotateLight =
+      glm::rotate(glm::mat4(1.f), 0.5f * frameInfo.frameTime, {0.f, -1.f, 0.f});
+  int lightIndex = 0;
+  for (auto &pointLight : context.pointLights) {
+    auto &transform = pointLight.transform;
 
-		PipelineConfigInfo pipelineConfig{};
-		LvePipeline::enableAlphaBlending(pipelineConfig);
-		pipelineConfig.attributeDescriptions.clear();
-		pipelineConfig.bindingDescriptions.clear();
-		pipelineConfig.renderPass = renderer::getSwapChainRenderPass(context);
-		pipelineConfig.pipelineLayout = pipelineLayout;
-		lvePipeline = std::make_unique<LvePipeline>(
-			context,
-			"shaders/point_light.vert.spv",
-			"shaders/point_light.frag.spv",
-			pipelineConfig);
-	}
+    assert(lightIndex < MAX_LIGHTS && "Point lights exceed maximum specified");
 
-	void PointLightSystem::update(FrameInfo& frameInfo, GlobalUbo& ubo) {
-		auto rotateLight = glm::rotate(glm::mat4(1.f), 0.5f * frameInfo.frameTime, { 0.f, -1.f, 0.f });
-		int lightIndex = 0;
-		for (auto& pointLight : context.pointLights) {
-			auto& transform = pointLight.transform;
+    // update light position
+    transform.translation =
+        glm::vec3(rotateLight * glm::vec4(transform.translation, 1.f));
 
-			assert(lightIndex < MAX_LIGHTS && "Point lights exceed maximum specified");
+    // copy light to ubo
+    ubo.pointLights[lightIndex].position =
+        glm::vec4(transform.translation, 1.f);
+    ubo.pointLights[lightIndex].color =
+        glm::vec4(pointLight.color, pointLight.lightIntensity);
 
-			// update light position
-			transform.translation = glm::vec3(rotateLight * glm::vec4(transform.translation, 1.f));
+    lightIndex += 1;
+  }
+  ubo.numLights = lightIndex;
+}
 
-			// copy light to ubo
-			ubo.pointLights[lightIndex].position = glm::vec4(transform.translation, 1.f);
-			ubo.pointLights[lightIndex].color = glm::vec4(pointLight.color, pointLight.lightIntensity);
+void render(EngineContext &context, FrameInfo &frameInfo) {
+  // sort lights
+  std::map<float, PointLight> sorted;
+  for (auto pointLight : context.pointLights) {
+    auto &transform = pointLight.transform;
 
-			lightIndex += 1;
-		}
-		ubo.numLights = lightIndex;
-	}
+    // calculate distance
+    auto offset = frameInfo.camera.getPosition() - transform.translation;
+    float disSquared = glm::dot(offset, offset);
+    sorted[disSquared] = pointLight;
+  }
 
-	void PointLightSystem::render(FrameInfo& frameInfo) {
-		// sort lights
-		std::map<float, PointLight> sorted;
-		for (auto pointLight : context.pointLights) {
-			auto& transform = pointLight.transform;
+  pipeline->bind(frameInfo.commandBuffer);
 
-			// calculate distance
-			auto offset = frameInfo.camera.getPosition() - transform.translation;
-			float disSquared = glm::dot(offset, offset);
-			sorted[disSquared] = pointLight;
-		}
+  vkCmdBindDescriptorSets(frameInfo.commandBuffer,
+                          VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1,
+                          &frameInfo.globalDescriptorSet, 0, nullptr);
 
-		lvePipeline->bind(frameInfo.commandBuffer);
+  // iterate through sorted lights in reverse order
+  for (auto it = sorted.rbegin(); it != sorted.rend(); ++it) {
+    // use game obj id to find light object
+    auto &pointLight = it->second;
+    auto &transform = pointLight.transform;
+    ;
 
-		vkCmdBindDescriptorSets(
-			frameInfo.commandBuffer,
-			VK_PIPELINE_BIND_POINT_GRAPHICS,
-			pipelineLayout,
-			0,
-			1,
-			&frameInfo.globalDescriptorSet,
-			0,
-			nullptr);
+    PointLightPushConstants push{};
+    push.position = glm::vec4(transform.translation, 1.f);
+    push.color = glm::vec4(pointLight.color, pointLight.lightIntensity);
+    push.radius = transform.scale.x;
 
-		// iterate through sorted lights in reverse order
-		for (auto it = sorted.rbegin(); it != sorted.rend(); ++it) {
-			// use game obj id to find light object
-			auto& pointLight = it->second;
-			auto& transform = pointLight.transform;;
-
-			PointLightPushConstants push{};
-			push.position = glm::vec4(transform.translation, 1.f);
-			push.color = glm::vec4(pointLight.color, pointLight.lightIntensity);
-			push.radius = transform.scale.x;
-
-			vkCmdPushConstants(
-				frameInfo.commandBuffer,
-				pipelineLayout,
-				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-				0,
-				sizeof(PointLightPushConstants),
-				&push);
-			vkCmdDraw(frameInfo.commandBuffer, 6, 1, 0, 0);
-		}
-	}
-
-}  // namespace lve
+    vkCmdPushConstants(frameInfo.commandBuffer, pipelineLayout,
+                       VK_SHADER_STAGE_VERTEX_BIT |
+                           VK_SHADER_STAGE_FRAGMENT_BIT,
+                       0, sizeof(PointLightPushConstants), &push);
+    vkCmdDraw(frameInfo.commandBuffer, 6, 1, 0, 0);
+  }
+}
+} // namespace pointLightSys
+} // namespace vkh
--- src/vkh/systems/point_light_system.hpp
+++ src/vkh/systems/point_light_system.hpp
@@ -1,33 +1,18 @@
 #pragma once
 
 #include "../engineContext.hpp"
-#include "../camera.hpp"
 #include "../frameInfo.hpp"
-#include "../gameObject.hpp"
 #include "../pipeline.hpp"
 
 #include <memory>
 #include <vector>
 
 namespace vkh {
-	class PointLightSystem {
-	public:
-		PointLightSystem(EngineContext& context, VkDescriptorSetLayout globalSetLayout);
-		~PointLightSystem();
+namespace pointLightSys {
+void init(EngineContext &context, VkDescriptorSetLayout globalSetLayout);
+void cleanup(EngineContext& context);
 
-		PointLightSystem(const PointLightSystem&) = delete;
-		PointLightSystem& operator=(const PointLightSystem&) = delete;
-
-		void update(FrameInfo& frameInfo, GlobalUbo& ubo);
-		void render(FrameInfo& frameInfo);
-
-	private:
-		void createPipelineLayout(VkDescriptorSetLayout globalSetLayout);
-		void createPipeline();
-
-		EngineContext& context;
-
-		std::unique_ptr<LvePipeline> lvePipeline;
-		VkPipelineLayout pipelineLayout;
-	};
-}  // namespace lve
+void update(EngineContext& context, FrameInfo &frameInfo, GlobalUbo &ubo);
+void render(EngineContext& context, FrameInfo &frameInfo);
+}; // namespace pointLightSys
+} // namespace vkh
