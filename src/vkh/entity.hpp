8ee2abbf6872e9e0ba8bad6ea3be0543627876a5
kekw <pboursin2008@orange.fr>
2024-11-02 16:39:08 +0100

    a

--- src/vkh/entity.hpp
+++ src/vkh/entity.hpp
@@ -12,44 +12,39 @@
 #include "engineContext.hpp"
 
 namespace vkh {
+
 class Model;
+
+// Transform structure for position, scale, and orientation
 struct Transform {
-  glm::vec3 position{};
-  glm::vec3 scale{1.f, 1.f, 1.f};
-  glm::quat orientation{};
+    glm::vec3 position{};
+    glm::vec3 scale{1.f, 1.f, 1.f};
+    glm::quat orientation{};
 
-  // Matrix corrsponds to Translate * Ry * Rx * Rz * Scale
-  // Rotations correspond to Tait-bryan angles of Y(1), X(2), Z(3)
-  // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
-  glm::mat4 mat4();
-
-  glm::mat3 normalMatrix();
+    glm::mat4 mat4();
+    glm::mat3 normalMatrix();
 };
 
+// Point light structure
 struct PointLight {
-  glm::vec3 color{};
-  float lightIntensity = 1.0f;
-  Transform transform;
+    glm::vec3 color{};
+    float lightIntensity = 1.0f;
+    Transform transform;
 };
 
-struct BoundingBox {
-  glm::vec3 min;
-  glm::vec3 max;
-  bool contains(const glm::vec3 &point) const {
-    return (point.x >= min.x && point.x <= max.x) &&
-           (point.y >= min.y && point.y <= max.y) &&
-           (point.z >= min.z && point.z <= max.z);
-  }
-  glm::vec3 center() const { return (min + max) * 0.5f; }
-  glm::vec3 size() const { return max - min; }
+// RigidBody structure for physics properties
+struct RigidBody {
+    glm::vec3 velocity{0.f};
+    float mass{1.f};
+    bool isStatic{false}; // Static bodies do not move
+
+    const glm::vec3 computeWeight() const { return {0, mass * 9.81f, 0}; }
 };
 
 struct Entity {
-  Transform transform;
-  std::shared_ptr<Model> model;
-  BoundingBox boundingBox;
-  glm::vec3 velocity{0.f};
-  float mass{1.f};
-  glm::vec3 computeWeight() { return {0, mass * 9.81, 0}; }
+    Transform transform;
+    std::shared_ptr<Model> model;
+    RigidBody rigidBody;
 };
+
 } // namespace vkh
