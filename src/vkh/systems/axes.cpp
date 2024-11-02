0dc09f43da182a852d24efe92218936ca0e4e065
Pierre Boursin <pboursin2008@orange.fr>
2024-11-02 15:51:56 +0100

    made stuff work under windows

--- src/vkh/systems/axes.cpp
+++ src/vkh/systems/axes.cpp
@@ -1,11 +1,10 @@
 #include "axes.hpp"
-#include <fmt/base.h>
 
-// libs
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
+#include <fmt/format.h>
 
 #include <cassert>
 #include <stdexcept>
