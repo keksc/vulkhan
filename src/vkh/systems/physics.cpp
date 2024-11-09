a60bd5fd26c5a94e27e8fcdffc3fe06a68fee3b1
Pierre Boursin <pboursin2008@orange.fr>
2024-11-09 12:13:45 +0100

    updated physics

--- src/vkh/systems/physics.cpp
+++ src/vkh/systems/physics.cpp
@@ -7,9 +7,9 @@ namespace vkh {
 namespace physicsSys {
 void update(EngineContext &context) {
   for (auto &entity : context.entities) {
-    if (entity.model == nullptr)
-      fmt::print("player velocity XYZ: {}, {}, {}\n", entity.rigidBody.velocity.x, entity.rigidBody.velocity.y, entity.rigidBody.velocity.x);
-    glm::vec3 force = entity.rigidBody.computeWeight();
+      if (entity.model == nullptr)
+          continue;
+    glm::vec3 force = entity.rigidBody.computeWeight() + glm::vec3{1.f, 0.f, 0.f};
     glm::vec3 acceleration = force / entity.rigidBody.mass;
     entity.rigidBody.velocity += acceleration * context.frameInfo.dt;
     entity.transform.position += entity.rigidBody.velocity * context.frameInfo.dt;
