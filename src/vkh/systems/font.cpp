554b8aa0db66d1948c8d6191415bb8a3404c1c7d
kekw <pboursin2008@orange.fr>
2024-12-28 18:48:15 +0100

    font

--- src/vkh/systems/font.cpp
+++ src/vkh/systems/font.cpp
@@ -19,7 +19,6 @@
 
 #include "../descriptors.hpp"
 #include "../deviceHelpers.hpp"
-#include "../entity.hpp"
 #include "../model.hpp"
 #include "../pipeline.hpp"
 #include "../renderer.hpp"
@@ -29,9 +28,23 @@ namespace fontSys {
 std::unique_ptr<Pipeline> pipeline;
 VkPipelineLayout pipelineLayout;
 
-stbtt_fontinfo fontInfo;
 std::unique_ptr<Model> model;
-std::vector<std::shared_ptr<Image>> glyphs;
+
+std::vector<char> fontDataChar;
+unsigned char *fontData;
+
+struct Glyph {
+  glm::vec2 size;
+  glm::vec2 offset;
+  glm::vec2 uvOffset;
+  glm::vec2 uvExtent;
+  float advance;
+};
+
+std::string text = "abcd";
+
+std::unordered_map<char, Glyph> glyphs;
+std::shared_ptr<Image> fontAtlas;
 
 // struct PushConstantData {};
 
@@ -75,38 +88,81 @@ void createPipeline(EngineContext &context) {
       "shaders/font.frag.spv", pipelineConfig);
 }
 void initFont() {
-  std::vector<char> fontDataChar = readFile("fonts/Roboto-Regular.ttf");
-  auto fontData = reinterpret_cast<unsigned char *>(fontDataChar.data());
-  stbtt_InitFont(&fontInfo, fontData, stbtt_GetFontOffsetForIndex(fontData, 0));
+  fontDataChar = readFile("fonts/Roboto-Regular.ttf");
+  fontData = reinterpret_cast<unsigned char *>(fontDataChar.data());
 }
-void createGlyphImages(EngineContext &context) {
-  char c = 'a';
-  int w, h, xoff, yoff;
-  unsigned char *bitmap = stbtt_GetGlyphBitmap(
-      &fontInfo, 1.0, 1.0, stbtt_FindGlyphIndex(&fontInfo, c), &w, &h, &xoff,
-      &yoff);
-  VkDeviceSize imageSize = w * h;
-  glyphs.push_back(std::make_shared<Image>(
-      context, fmt::format("glyph {}", static_cast<char>(c)), w, h, bitmap,
-      VK_FORMAT_R8_UNORM));
-  stbtt_FreeBitmap(bitmap, nullptr);
+void createGlyphs(EngineContext &context) {
+  const glm::vec2 bitmapExtent = {512.f, 512.f};
+  const int atlasSize = bitmapExtent.x * bitmapExtent.y;
+  const float fontSize = 32.f;
+
+  unsigned char *atlasData = new unsigned char[atlasSize];
+  std::memset(atlasData, 0, atlasSize);
+
+  stbtt_pack_context packContext;
+  if (!stbtt_PackBegin(&packContext, atlasData, bitmapExtent.x, bitmapExtent.y,
+                       0, 1, nullptr)) {
+    throw std::runtime_error("Failed to initialize font packing");
+  }
+  std::vector<stbtt_packedchar> charInfo(127 - 32);
+  stbtt_PackSetOversampling(&packContext, 1, 1);
+  if (!stbtt_PackFontRange(&packContext, fontData, 0, fontSize, 32,
+                           charInfo.size(), charInfo.data())) {
+    stbtt_PackEnd(&packContext);
+    delete[] atlasData;
+    throw std::runtime_error("Failed to pack font");
+  }
+  stbtt_PackEnd(&packContext);
+
+  fontAtlas =
+      std::make_shared<Image>(context, "font atlas", bitmapExtent.x,
+                              bitmapExtent.y, atlasData, VK_FORMAT_R8_UNORM);
+
+  for (int i = 0; i < charInfo.size(); i++) {
+    char c = i + 32;
+    const stbtt_packedchar &pc = charInfo[i];
+
+    glm::vec2 size = {(pc.xoff2 - pc.xoff)/context.vulkan.swapChain->getSwapChainExtent().width, (pc.yoff2 - pc.yoff)/context.vulkan.swapChain->getSwapChainExtent().height};
+    glm::vec2 offset = {pc.xoff, pc.yoff};
+    Glyph glyph{.size = size,
+                .offset = offset,
+                .uvOffset = glm::vec2(pc.x0, pc.y0) / bitmapExtent,
+                .uvExtent =
+                    glm::vec2(pc.x1 - pc.x0, pc.y1 - pc.y0) / bitmapExtent,
+                .advance = pc.xadvance};
+
+    glyphs[c] = glyph;
+  }
+  delete[] atlasData;
 }
-inline int getCharIndexInGlyphs(char c) { return c + 32; }
 void init(EngineContext &context) {
   initFont();
-  createGlyphImages(context);
+  createGlyphs(context);
 
   glm::vec3 col(0.0);
   glm::vec3 n(0.0);
+  auto ch = glyphs['b'];
+  float scale = 0.01f; // Adjust this value as needed
+
   std::vector<Model::Vertex> vertices = {
-      {{-1.0, -1.0, 0.0}, col, n, {-1.0, -1.0}},
-      {{1.0, -1.0, 0.0}, col, n, {1.0, -1.0}},
-      {{1.0, 1.0, 0.0}, col, n, {1.0, 1.0}},
-      {{-1.0, 1.0, 0.0}, col, n, {-1.0, 1.0}},
+      {{0.f, 0.f, 0.0}, col, n, {ch.uvOffset.x, ch.uvOffset.y}},
+      {{ch.size.x, 0.f, 0.0},
+       col,
+       n,
+       {ch.uvOffset.x + ch.uvExtent.x, ch.uvOffset.y}},
+      {{ch.size.x, ch.size.y, 0.0},
+       col,
+       n,
+       {ch.uvOffset.x + ch.uvExtent.x, ch.uvOffset.y + ch.uvExtent.y}},
+      {{0.f, ch.size.y, 0.0},
+       col,
+       n,
+       {ch.uvOffset.x, ch.uvOffset.y + ch.uvExtent.y}},
   };
   std::vector<uint32_t> indices = {0, 1, 2, 0, 3, 2};
-  model = std::make_unique<Model>(context, "font model", vertices, indices,
-                                  glyphs[0]);
+
+  model = std::make_unique<Model>(context, "font atlas", vertices, indices,
+                                  fontAtlas);
 
   createPipelineLayout(context);
   createPipeline(context);
@@ -115,6 +171,7 @@ void init(EngineContext &context) {
 void cleanup(EngineContext &context) {
   pipeline = nullptr;
   glyphs.clear();
+  fontAtlas = nullptr;
   model = nullptr;
   vkDestroyPipelineLayout(context.vulkan.device, pipelineLayout, nullptr);
 }
@@ -122,15 +179,12 @@ void cleanup(EngineContext &context) {
 void render(EngineContext &context) {
   pipeline->bind(context.frameInfo.commandBuffer);
 
-  /*vkCmdBindDescriptorSets(context.frameInfo.commandBuffer,
-                          VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1,
-                          &context.frameInfo.globalDescriptorSet, 0, nullptr);*/
-
   /*PushConstantData push{};
 
   vkCmdPushConstants(context.frameInfo.commandBuffer, pipelineLayout,
                      VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
                      0, sizeof(PushConstantData), &push);*/
+
   model->bind(context, context.frameInfo.commandBuffer, pipelineLayout);
   model->draw(context.frameInfo.commandBuffer);
 }
