2653e113f406a489d3459d2f0216d5c9dc88b64a
M F <mf@example.com>
2025-11-05 20:16:13 +0100

    my trash, please fix it

--- src/sabl.hpp
+++ src/sabl.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "vkh/systems/entity/entities.hpp"
+#include "vkh/engineContext.hpp"
 
+// this class contains Sabl(), the constructor 
+// the Scene (uninteresting),
+// The snake pieces for both players,
+// the velocities for both players' heads.
+//
+// this is a bad implementation, much worse than my friends' that is not an approximation
+// and can be smoothed in replay.
 class Sabl {
 public:
   Sabl(vkh::EngineContext &context, vkh::EntitySys &entitySys);
+  ~Sabl();
   void update();
+private:
+  vkh::EntitySys& entitySys;
+  vkh::EngineContext& context;
+
   std::shared_ptr<vkh::Scene<vkh::EntitySys::Vertex>> sandwormScene;
   std::vector<std::shared_ptr<vkh::EntitySys::Entity>> leafSnakePieces;
   std::vector<std::shared_ptr<vkh::EntitySys::Entity>> kekwSnakePieces;
